415d17c83032796c8243fe63d1bd952b72d486fd
Mohammed Karim <mkarim@jpl.nasa.gov>
2022-12-22 13:56:01 -0800

    update

--- lib/merge_neighbors_atl08.py
+++ lib/merge_neighbors_atl08.py
@@ -3,7 +3,7 @@
 import os
 import pandas as pd
 import geopandas
-
+import traceback
 import argparse
 
 def local_to_s3(url, user = 'nathanmthomas', type='public'):
@@ -97,7 +97,15 @@ def main():
     ATL08_filt_csv_s3_fn_list = [item for sublist in ATL08_filt_csv_s3_fn_list for item in sublist]
     
     # Read these ATL08 filtered CSVs into a single df
-    atl08 = pd.concat([pd.read_csv(f) for f in ATL08_filt_csv_s3_fn_list], sort=False)
+    # atl08 = pd.concat([pd.read_csv(f) for f in ATL08_filt_csv_s3_fn_list], sort=False)
+	for f in ATL08_filt_csv_s3_fn_list:
+		print(f)
+		try:
+		    pd.concat(read_csv(f), sort=False)
+		except Exception as e:
+		    str(e)
+			traceback.print_stack()
+	    
     
     # Write df to CSV
     out_csv_fn = os.path.join(out_dir, "atl08_004_30m_filt_merge_neighbors_" + str(f'{in_tile_num:04}.csv') )
