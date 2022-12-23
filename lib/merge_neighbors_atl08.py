6fb7572757fec92258cdfc44c10fa660f7f56b1b
Mohammed Karim <mkarim@jpl.nasa.gov>
2022-12-23 10:24:51 -0800

    update

--- lib/merge_neighbors_atl08.py
+++ lib/merge_neighbors_atl08.py
@@ -100,7 +100,6 @@ def main():
     # Read these ATL08 filtered CSVs into a single df
     atl08 = pd.concat([pd.read_csv(f) for f in ATL08_filt_csv_s3_fn_list], sort=False)
 	    
-    print(pd)
     # Write df to CSV
 	print("Outdir : {}".format(out_dir))
     out_csv_fn = os.path.join(out_dir, "atl08_004_30m_filt_merge_neighbors_" + str(f'{in_tile_num:04}.csv') )
