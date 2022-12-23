06024ffed3d09370ab4318f8d463d5cc8082fe58
Mohammed Karim <mkarim@jpl.nasa.gov>
2022-12-22 18:31:34 -0800

    temp print command

--- lib/merge_neighbors_atl08.py
+++ lib/merge_neighbors_atl08.py
@@ -96,16 +96,17 @@ def main():
     # Convert from list of lists to list
     ATL08_filt_csv_s3_fn_list = [item for sublist in ATL08_filt_csv_s3_fn_list for item in sublist]
    
-    print(ATL08_filt_csv_s3_fn_list) 
-    # Read these ATL08 filtered CSVs into a single df
-    # atl08 = pd.concat([pd.read_csv(f) for f in ATL08_filt_csv_s3_fn_list], sort=False)
+    print("ATL08_filt_csv_s3_fn_list : {}".format(ATL08_filt_csv_s3_fn_list))
 	for f in ATL08_filt_csv_s3_fn_list:
 		print("Processing : {}".format(f))
 		try:
 		    pd.concat(pd.read_csv(f), sort=False)
 		except Exception as e:
-		    str(e)
+		    print(str(e))
 			traceback.print_stack()
+
+    # Read these ATL08 filtered CSVs into a single df
+    # atl08 = pd.concat([pd.read_csv(f) for f in ATL08_filt_csv_s3_fn_list], sort=False)
 	    
     
     # Write df to CSV
